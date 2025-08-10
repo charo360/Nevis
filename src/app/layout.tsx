90d755b5299a9cee28510f625c396660b46d2e33
Samuel Mweni <sarcharo@gmail.com>
2025-08-10 22:45:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,9 +70,6 @@ export default function RootLayout({
       <head>
         <title>LocalBuzz</title>
         <meta name="description" content="Hyper-local, relevant social media content generation for local businesses" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
           <SidebarProvider>
